ca09da9e0115de2c11d4960b1a44521c9a3b4d4e
haseok2118 <66732566+haseok2118@users.noreply.github.com>
2020-11-12 17:20:08 +0900

    [Client] feat/login_page
    로그인 페이지 구현

--- bookshare/src/App.js
+++ bookshare/src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
+// import './App.css';
+import React from "react";
 import { Route } from 'react-router-dom';
 import Main from './MainPage/Main';
 import AuthMain from './AuthPage/AuthMain';
 function App() {
 	return (
 		<div className="App">
-			<Route exact path="/" component={Main} />
-			<Route path="/about" component={AuthMain} />
+			<Route exact path="/about" component={AuthMain} />
+			<Route path="/" component={Main} />
 		</div>
 	);
 }
