930a59f772952645351aa1514e1a79fefeb46d49
chiione choi <chiionechoi@chiioneui-MacBookAir.local>
2020-11-12 17:53:02 +0900

    feat/mainPage

--- bookshare/src/App.js
+++ bookshare/src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import { Route } from 'react-router-dom';
 import Main from './MainPage/Main';
 import AuthMain from './AuthPage/AuthMain';
+import Nav from './Nav';
+
 function App() {
 	return (
 		<div className="App">
-			<Route exact path="/" component={Main} />
+			<Route exact path="/:id" component={Main} />
+			<Route exact path="/:id/:name" component={Nav}></Route>
 			<Route path="/about" component={AuthMain} />
 		</div>
 	);
